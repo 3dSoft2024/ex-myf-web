731281d4320fb916f892cf6906edf0746e292f40
vgamarra2021 <vgamarrav@uni.pe>
2024-04-16 14:19:31 -0500

    Update title logo name

--- src/components/HeaderGeneral.vue
+++ src/components/HeaderGeneral.vue
@@ -1,45 +1,44 @@
 <template lang="html">
-    <div class="d-flex flex-row justify-content-center aling-items-center bg-dark py-2">
-        <div class="container d-flex flex-row justify-content-between">
-            <div class="logo">
-                <img class="logo" src="@/assets/logo_blanco.svg">
-            </div>
-            <nav class="d-flex flex-row align-items-center gap-3 dropdown">
-                <router-link class="nav-link d-flex flex-row align-items-center" to="/">
-                    <p class="m-0 text-light">INICIO</p>
-                </router-link>
-                <div class="nav-link d-flex flex-row align-items-center dropdown-toggle text-light" id="services" data-bs-toggle="dropdown" aria-expanded="false">
-                    <p class="m-0 text-light ">SERVICIOS</p>
-                </div>
-                <ul class="dropdown-menu" aria-labelledby="services">
-                    <li><router-link class="dropdown-item" to="/parabrisas">Reparación de Parabrisas</router-link></li>
-                    <li><router-link class="dropdown-item" to="/faros">Pulido de Faros</router-link></li>
-                    <li><router-link class="dropdown-item" to="/vidrios">Pulido de Vidrios</router-link></li>
-                    <li><router-link class="dropdown-item" to="/repelente">Repelente Hidrofóbico</router-link></li>
-                </ul>
-                <router-link class="nav-link d-flex flex-row align-items-center" to="/galeria">
-                    <p class="m-0 text-light">GALERIA</p>
-                </router-link>
-                <router-link class="nav-link d-flex flex-row align-items-center" to="/contacto">
-                    <p class="m-0 text-light">CONTACTO</p>
-                </router-link>
-                <div class="linea bg-light"></div>
-                <div class="redes d-flex flex-row pb-2">
-                    <a href="https://www.facebook.com/Restauraciondeparabrisasypulidodevidrioss">
-                        <img src="@/assets/fb.svg" alt="fb" class="mx-2 ms-3">
-                    </a>
-                    <a href="https://twitter.com/ParabrisasDe?s=09">
-                        <img src="@/assets/twitter.svg" alt="twitter" class="mx-2">
-                    </a>
-                    <a href="https://vm.tiktok.com/ZMLW9HoCk/">
-                        <img src="@/assets/tiktok.svg" alt="tiktok" class="mx-2">
-                    </a>
-                </div>
-
-            </nav>
-            
+  <div class="d-flex flex-row justify-content-center aling-items-center bg-dark py-2">
+    <div class="container d-flex flex-row justify-content-between">
+      <div class="d-flex flex-row gap-2 align-items-center justify-content-center p-1">
+        <img class="logo" src="@/assets/logo_blanco.svg" />
+        <p style="font-family: 'Trade Winds', cursive; max-width: 300px; font-size: 1rem" class="m-0 text-light">Reparacion de Parabrisas y Pulido de Vidrios/Faros - M&F</p>
+      </div>
+      <nav class="d-flex flex-row align-items-center gap-3 dropdown">
+        <router-link class="nav-link d-flex flex-row align-items-center" to="/">
+          <p class="m-0 text-light">INICIO</p>
+        </router-link>
+        <div class="nav-link d-flex flex-row align-items-center dropdown-toggle text-light" id="services" data-bs-toggle="dropdown" aria-expanded="false">
+          <p class="m-0 text-light">SERVICIOS</p>
         </div>
+        <ul class="dropdown-menu" aria-labelledby="services">
+          <li><router-link class="dropdown-item" to="/parabrisas">Reparación de Parabrisas</router-link></li>
+          <li><router-link class="dropdown-item" to="/faros">Pulido de Faros</router-link></li>
+          <li><router-link class="dropdown-item" to="/vidrios">Pulido de Vidrios</router-link></li>
+          <li><router-link class="dropdown-item" to="/repelente">Repelente Hidrofóbico</router-link></li>
+        </ul>
+        <router-link class="nav-link d-flex flex-row align-items-center" to="/galeria">
+          <p class="m-0 text-light">GALERIA</p>
+        </router-link>
+        <router-link class="nav-link d-flex flex-row align-items-center" to="/contacto">
+          <p class="m-0 text-light">CONTACTO</p>
+        </router-link>
+        <div class="linea bg-light"></div>
+        <div class="redes d-flex flex-row pb-2">
+          <a href="https://www.facebook.com/Restauraciondeparabrisasypulidodevidrioss">
+            <img src="@/assets/fb.svg" alt="fb" class="mx-2 ms-3" />
+          </a>
+          <a href="https://twitter.com/ParabrisasDe?s=09">
+            <img src="@/assets/twitter.svg" alt="twitter" class="mx-2" />
+          </a>
+          <a href="https://vm.tiktok.com/ZMLW9HoCk/">
+            <img src="@/assets/tiktok.svg" alt="tiktok" class="mx-2" />
+          </a>
+        </div>
+      </nav>
     </div>
+  </div>
 </template>
 
 <script>
@@ -47,28 +46,28 @@ export default {};
 </script>
 
 <style lang="sass" scoped>
-    .dropdown-item
+.dropdown-item
+    &:hover
+        color: #fff
+        background: #212529
+.router-link-exact-active
+    background: rgba(255, 255, 255, 0.18)
+.redes
+    img
+        height: 25px
         &:hover
-            color: #fff
-            background: #212529
-    .router-link-exact-active
-        background: rgba(255, 255, 255, 0.18)
-    .redes
-        img
-            height: 25px
-            &:hover
-                opacity: 0.6
-                transition: all 0.1s ease
-    .logo
-        height: 60px
-    .linea
-        height: 80%
-        width: 0.5px
-        opacity: 0.5
+            opacity: 0.6
+            transition: all 0.1s ease
+.logo
+    height: 50px
+.linea
+    height: 80%
+    width: 0.5px
+    opacity: 0.5
 </style>
 <style lang="sass" scoped>
 @media (max-width: 1025px)
-  .container 
+  .container
     flex-direction: column !important
     .logo
         margin-bottom: 10px
@@ -78,6 +77,4 @@ export default {};
 @media (max-width: 600px)
   .nav-link, .linea
       display: none !important
-    
-    
 </style>
